83f3f6e1db868b2615a7f7619de0f947b6b4b73e
Rachel Pizane <rachel.pzm@gmail.com>
2025-11-01 17:34:33 -0300

    feat: adicionar chamada de teste para validar integração inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProdutoService } from './core/services/produto.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'confeitaria-app';
+export class AppComponent implements OnInit {
+  title: string = 'confeitaria-app';
+  produto?: string;
+  constructor(private produtoService: ProdutoService) {}
+
+  ngOnInit() {
+    this.getProduto(1);
+  }
+
+  getProduto(id: number) {
+    this.produtoService.getProdutoById(id).subscribe({
+      next: (data) => {
+        console.log('Produto recebido:', data.mensagem);
+        this.produto = data.mensagem
+      },
+      error: (error) => {
+        console.error('Erro ao buscar produto:', error);
+      },
+    });
+  }
 }
